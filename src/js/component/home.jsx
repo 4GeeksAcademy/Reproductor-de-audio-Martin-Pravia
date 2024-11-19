ee74216249d691ffd5021af90a284804cd65741f
Martín Pravia <marto.pravia@gmail.com>
2024-11-19 17:34:44 +0000

    queda la estructura realizada, pronto para comenzar a hacer el fetch con la api

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,41 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <>
+      <div className="text-start">
+        <ol class="list-group list-group-numbered">
+          <li class="list-group-item list-group-item-action list-group-item-success">
+            A list item
+          </li>
+          <li class="list-group-item list-group-item-action list-group-item-success">
+            A list item
+          </li>
+          <li class="list-group-item list-group-item-action list-group-item-success">
+            A list item
+          </li>
+        </ol>
+      </div>
+      <footer className="bg-dark text-center py-2 position-fixed bottom-0 w-100">
+        <div className="d-flex justify-content-around align-items-center">
+          <button className="btn btn-secondary">
+            <i class="bi bi-skip-backward-btn-fill"></i>
+          </button>
+          <button className="btn btn-secondary">
+            <i class="bi bi-pause-btn-fill"></i>
+          </button>
+          <button className="btn btn-secondary">
+            <i class="bi bi-play-btn-fill"></i>
+          </button>
+          <button className="btn btn-secondary">
+            <i class="bi bi-skip-forward-btn-fill"></i>
+          </button>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Home;
