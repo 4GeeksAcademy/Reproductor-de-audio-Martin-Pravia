be7a11534280767db7faf54592bcfc81df180689
Martín Pravia <marto.pravia@gmail.com>
2024-11-19 19:09:13 +0000

    se hace el fetch a la url y traemos toda la data

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,36 +1,53 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetch("https://playground.4geeks.com/sound/songs")
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Ha ocurrido un error");
+        }
+        return response.json();
+      })
+      .then((dataRecived) => {
+        console.log(dataRecived);
+        setSongs(dataRecived.songs);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  }, []);
+
   return (
     <>
       <div className="text-start">
-        <ol class="list-group list-group-numbered">
-          <li class="list-group-item list-group-item-action list-group-item-success">
-            A list item
-          </li>
-          <li class="list-group-item list-group-item-action list-group-item-success">
-            A list item
-          </li>
-          <li class="list-group-item list-group-item-action list-group-item-success">
-            A list item
-          </li>
+        <ol className="list-group list-group-numbered">
+          {songs.map((song, index) => (
+            <li
+              key={song.id}
+              className="list-group-item list-group-item-action list-group-item-success"
+            >
+              {song.name}
+            </li>
+          ))}
         </ol>
       </div>
       <footer className="bg-dark text-center py-2 position-fixed bottom-0 w-100">
         <div className="d-flex justify-content-around align-items-center">
           <button className="btn btn-secondary">
-            <i class="bi bi-skip-backward-btn-fill"></i>
+            <i className="bi bi-skip-backward-btn-fill"></i>
           </button>
           <button className="btn btn-secondary">
-            <i class="bi bi-pause-btn-fill"></i>
+            <i className="bi bi-pause-btn-fill"></i>
           </button>
           <button className="btn btn-secondary">
-            <i class="bi bi-play-btn-fill"></i>
+            <i className="bi bi-play-btn-fill"></i>
           </button>
           <button className="btn btn-secondary">
-            <i class="bi bi-skip-forward-btn-fill"></i>
+            <i className="bi bi-skip-forward-btn-fill"></i>
           </button>
         </div>
       </footer>
